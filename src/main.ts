845e7e276a92c3864ca3d478aeb21c110a33557a
htz <1075898441@qq.com>
2024-05-30 15:29:36 +0800

    feat: addnew

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
+/*
+ * @Author: htz
+ * @Date: 2024-05-29 16:14:59
+ * @LastEditors: 
+ * @LastEditTime: 2024-05-30 15:29:10
+ * @Description: 请填写简介
+ */
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
